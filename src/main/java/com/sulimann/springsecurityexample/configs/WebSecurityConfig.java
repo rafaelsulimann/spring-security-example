da0b68368b13c0459c1da8806f9c92e4b5bb4902
Rafael Sulimann <sulimannrafael@gmail.com>
2024-05-01 20:37:30 -0300

    feat: Configuração de CORS

--- src/main/java/com/sulimann/springsecurityexample/configs/WebSecurityConfig.java
+++ src/main/java/com/sulimann/springsecurityexample/configs/WebSecurityConfig.java
@@ -1,17 +1,43 @@
 package com.sulimann.springsecurityexample.configs;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebSecurityConfig {
 
+  @Value("${cors.origins}")
+  private String corsOrigins;
+
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
+    http.cors(cors -> cors.configurationSource(this.corsConfigurationSource()));
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+
+    String[] origins = this.corsOrigins.split(",");
+
+    CorsConfiguration corsConfig = new CorsConfiguration();
+    corsConfig.setAllowedOriginPatterns(Arrays.asList(origins));
+    corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+    corsConfig.setAllowCredentials(true);
+    corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfig);
+    return source;
   }
 
 }
