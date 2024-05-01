46d7df951f6c7a12deca7a49c2610606a24b7ff8
Rafael Sulimann <sulimannrafael@gmail.com>
2024-05-01 19:45:07 -0300

    feat: Adicionado dependência spring security

--- src/main/java/com/sulimann/springsecurityexample/utils/constants/Regex.java
+++ src/main/java/com/sulimann/springsecurityexample/utils/constants/Regex.java
@@ -6,6 +6,7 @@ public final class Regex {
     public static final String CPF_OU_CNPJ = "(\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2})|(\\d{2}\\.?\\d{3}\\.?\\d{3}/?\\d{4}-?\\d{2})";
     public static final String CEP = "\\d{5}-?\\d{3}";
     public static final String CELULAR = "(\\(\\d{2}\\))?\\d{2}[- ]?\\d{4,5}[- ]?\\d{4}";
+    public static final String SENHA_LIMPA = "^\\$2[ayb]\\$\\d{2}\\$[./0-9A-Za-z]{53}$";
 
     private Regex() {
         throw new AssertionError("Não é permitido instanciar esta classe.");
