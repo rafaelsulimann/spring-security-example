9c243ff138275e1558869b443e37d3c1356edb12
Rafael Sulimann <sulimannrafael@gmail.com>
2024-05-01 23:17:08 -0300

    feat: Basic Auth + Obrigar autenticação para alguns endpoints e permitir para outros

--- src/main/java/com/sulimann/springsecurityexample/configs/WebSecurityConfig.java
+++ src/main/java/com/sulimann/springsecurityexample/configs/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,8 +20,12 @@ public class WebSecurityConfig {
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
-    http.cors(cors -> cors.configurationSource(this.corsConfigurationSource()));
+    http
+      .authorizeHttpRequests(authz -> authz
+        .requestMatchers("/init", "/init/**").authenticated()
+        .anyRequest().permitAll())
+      .httpBasic(Customizer.withDefaults())
+      .cors(cors -> cors.configurationSource(this.corsConfigurationSource()));
     return http.build();
   }
 
